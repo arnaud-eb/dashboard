a390b8b52fbc7909662b070c49047c77be09893c
arnaud-eb <arnaud.depierreux@easebest.com>
2021-07-14 08:07:54 +0200

    Updates

---
+++ precache-manifest.13ccfa9a0ac39049bc0c930ce2b1f49d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dd95c35e88056676ffd0c9e941cea5",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "8369ac41c3fb39846b19",
+    "url": "/dashboard/static/css/main.79ba2640.chunk.css"
+  },
+  {
+    "revision": "067110fabc9cff23472f",
+    "url": "/dashboard/static/js/2.3c2f1fe1.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/dashboard/static/js/2.3c2f1fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8369ac41c3fb39846b19",
+    "url": "/dashboard/static/js/main.649c5a12.chunk.js"
+  },
+  {
+    "revision": "43b96949e7e2e666ba66",
+    "url": "/dashboard/static/js/runtime-main.69c30187.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/dashboard/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/dashboard/static/media/preloader.c114fda3.gif"
+  }
+]);
