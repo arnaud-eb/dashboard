fcb03f9dbb5d189b869a5edc4d5f60a0dc28a377
arnaud-eb <arnaud.depierreux@easebest.com>
2021-07-14 11:29:32 +0200

    Auth0 domain + Client ID

--- src/index.js
+++ src/index.js
@@ -8,17 +8,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-console.log(domain);
-console.log(clientId);
-console.log(window.location.origin);
 
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-o2oofajo.eu.auth0.com"
-      clientId="BkseMdS8tB2IFJeu8sXtM8QJrdBHJRpC"
+      domain={domain}
+      clientId={clientId}
       redirectUri={window.location.origin + "/dashboard/"}
-      cacheLocation="localstorage"
+      // cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
